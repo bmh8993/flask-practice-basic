3a3983c08fde363ad0f0e9ab0027d14d49473ab0
MinHyeokBae <bmh9175@gmail.com>
2021-02-13 09:57:43 +0900

    flask-restful 사용1

--- section4/app.py
+++ section4/app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {"student": name}
+        for item in items:
+            if item["name"] == name:
+                return item
+        return {"item": None}, 404
+
+    def post(self, name):
+        data = request.get_json()
+        # get_json의 옵션으로 force=True, silent=True가 있는데 force=True는 header의
+        # content-type을 무시한다.
+        # silent=True는 에러가 발생해도 None를 return 한다.
+        item = {'name': name, 'price': data["price"]}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
 
 
-api.add_resource(Student, "/student/<string:name>")  # http://127.0.0.1:5000/student/Rolf
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
