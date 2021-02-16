7b09119dd2418ed2d94cc18dd0b64082b178d76d
MinHyeokBae <bmh9175@gmail.com>
2021-02-16 22:59:09 +0900

    separate method

---
+++ section6/models/item.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+class ItemModel:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def json(self):
+        return {"name": self.name, "price": self.price}
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items where name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+        # that we've got the row out, we can do a connection.close because we no longer
+        # need the connection open
+
+        if row:
+            return cls(*row)
+
+        # this method is still class method, because this method is going to return
+        # dictionary an object of type ItemModel
+
+    def insert(self):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (self.name, self.price))
+
+        connection.commit()
+        connection.close()
+
+    def update(self):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (self.price, self.name))
+
+        connection.commit()
+        connection.close()
+
+# resources is endpoint that interact client such as web or mobile.
+# The change in methods created through classmethod does not directly affect API.
+# therefore, I will separate the methods.
--- section6/resources/user.py
+++ section6/resources/user.py
@@ -2,49 +2,8 @@ import sqlite3
 
 from flask_restful import Resource, reqparse
 
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
+from models.user import UserModel
 
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        # just get the first row, if there are no rows, result.fetchone() is going to return None
-        if row:
-            # user = cls(row[0], row[1], row[2])
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-
-        return user
-
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        # just get the first row, if there are no rows, result.fetchone() is going to return None
-        if row:
-            # user = cls(row[0], row[1], row[2])
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-
-        return user
 
 class UserRegister(Resource):
 
@@ -63,7 +22,7 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data["username"]) is not None:
+        if UserModel.find_by_username(data["username"]) is not None:
             return {"message": "A user with that username already exists"}, 400
         # Make sure to have this in front of the connection creatino line
 
