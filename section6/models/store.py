23a4ce811ec1b1380863ea8286fe701f4c8f226f
MinHyeokBae <bmh9175@gmail.com>
2021-02-22 23:32:42 +0900

    create store model with sqlalchemy using foreignky

--- section6/models/store.py
+++ section6/models/store.py
@@ -1,22 +1,23 @@
 from db import db
 
-class ItemModel(db.Model):
-    __tablename__ = "items"
+class StoreModel(db.Model):
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
+    items = db.relationship("ItemModel", lazy="dynamic")
 
-    def __init__(self, name, price, store_id):
+    # when we use lazy equals dynamic, self.items no longer is a list of items
+    # now it is a query builder that has the ability to look into the items table
+    # 쿼리가 자동으로 실행되지 않는다. 실제 실행까지 로딩이 있다.
+    # https://medium.com/@ns2586/sqlalchemys-relationship-and-lazy-parameter-4a553257d9ef
+
+    def __init__(self, name):
         self.name = name
-        self.price = price
-        self.store_id = store_id
 
     def json(self):
-        return {"name": self.name, "price": self.price}
+        return {"name": self.name, "items": [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
